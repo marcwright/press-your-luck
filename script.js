eb1a8589bbf31a0c0d23ebbaa9ec40ee7f0edd84
marcwright <marc.wright@gmail.com>
2016-03-30 10:24:50 -0400

    fixed background pos

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 var colorArray = [
-  {value: 470,   text: "470",    free_spin: 0, img: "luck_board.png", pos: "-2px -18px"},
-  {value: 750,   text: "750",    free_spin: 0, img: "luck_board.png", pos: "-2px -132px"},
-  {value: 1200,  text: "1200",   free_spin: 0, img: "luck_board.png", pos: "-2px -252px"},
-  {value: 1750,  text: "1750",   free_spin: 0, img: "luck_board.png", pos: "-2px -372px"},
-  {value: 2500,  text: "2500",   free_spin: 1, img: "luck_board.png", pos: "-2px -492px"},
-  {value: -1,    text: "WHAMMY", free_spin: 0, img: "whammy1.gif"},
-  {value: 2750,  text: "2750",   free_spin: 0, img: "luck_board.png", pos: "-101px -492px"},
-  {value: 3000,  text: "3000",   free_spin: 0, img: "luck_board.png", pos: "-198px -492px"},
-  {value: 3500,  text: "3500",    free_spin: 0, img: "luck_board.png", pos: "-395px -492px"},
-  {value: 4000,  text: "4000",   free_spin: 1, img: "luck_board.png", pos: "-589px -492px"},
-  {value: 2000,  text: "2000",   free_spin: 1, img: "luck_board.png", pos: "-394px -374px"},
-  {value: 1500,  text: "1500",   free_spin: 1, img: "luck_board.png", pos: "-492px -251px"},
-  {value: 1000,  text: "1000",   free_spin: 1, img: "luck_board.png", pos: "-492px -136px"},
-  {value: 1000,  text: "1000",   free_spin: 0, img: "luck_board.png", pos: "-394px -136px"}
-
-  // background-position: -2318px -465px;
-//   background-position: -36px -35px; 
-// width: 398px;
-// height: 409px;
+  {value: 470,   text: "470",    free_spin: 0, img: "luck_board_low_res.jpg", pos: "-13px -18px"},
+  {value: 750,   text: "750",    free_spin: 0, img: "luck_board_low_res.jpg", pos: "-13px -132px"},
+  {value: 1000,  text: "1000",   free_spin: 1, img: "luck_board_low_res.jpg", pos: "-582px -136px"},
+  {value: 1000,  text: "1000",   free_spin: 0, img: "luck_board_low_res.jpg", pos: "-469px -136px"},
+  {value: 1200,  text: "1200",   free_spin: 0, img: "luck_board_low_res.jpg", pos: "-13px -252px"},
+  {value: 1500,  text: "1500",   free_spin: 1, img: "luck_board_low_res.jpg", pos: "-468px -251px"},
+  {value: 1750,  text: "1750",   free_spin: 0, img: "luck_board_low_res.jpg", pos: "-13px -372px"},
+  {value: 2000,  text: "2000",   free_spin: 1, img: "luck_board_low_res.jpg", pos: "-468px -374px"},
+  // {value: 2500,  text: "2500",   free_spin: 1, img: "luck_board_low_res.jpg", pos: "-113px -492px"},
+  {value: 2750,  text: "2750",   free_spin: 0, img: "luck_board_low_res.jpg", pos: "-126px -492px"},
+  {value: 3000,  text: "3000",   free_spin: 0, img: "luck_board_low_res.jpg", pos: "-239px -492px"},
+  {value: 3500,  text: "3500",   free_spin: 0, img: "luck_board_low_res.jpg", pos: "-470px -492px"},
+  {value: 4000,  text: "4000",   free_spin: 1, img: "luck_board_low_res.jpg", pos: "-582px -492px"},
+  {value: -1,    text: "WHAMMY", free_spin: 0, img: "whammy2.png"},
+  {value: -1,    text: "WHAMMY", free_spin: 0, img: "whammy2.png"},
+  {value: -1,    text: "WHAMMY", free_spin: 0, img: "whammy2.png"},
 ];
 
 var score = 0;
@@ -41,7 +38,7 @@ function blink(){
       } else {
         $(this).css("background-image", "url(img/" + arraySelection.img + ")")
                .css('background-position', arraySelection.pos)
-               .css('background-size', "685px 600px")
+               .css('background-size', "800px 600px")
                .attr('value', arraySelection.value);
         
 
